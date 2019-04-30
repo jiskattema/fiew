0e42b1bf47686c2210b48f13e3935fa51a699e73
Jisk Attema <j.attema@esciencecenter.nl>
2019-04-30 07:43:35 +0200

    basic prototyping
    * read midi notes, and play
    * setup a canvas with some threejs content

--- src/components/Basic.vue
+++ src/components/Basic.vue
@@ -7,27 +7,77 @@
 
 module.exports = {
   data () {
-    return {}
+    var THREE = this.$THREE
+    var width = 800
+    var height = 600
+
+    var scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xcccccc)
+
+    var dirLight = new THREE.DirectionalLight(0xffffff, 0.125)
+    dirLight.position.set(0, 0, 1).normalize()
+    scene.add(dirLight)
+
+    var pointLight = new THREE.PointLight(0xffffff, 1.5)
+    pointLight.position.set(0, 100, 90)
+    scene.add(pointLight)
+
+    var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
+    camera.position.x = 0.0
+    camera.position.y = 0.0
+    camera.position.z = 500.0
+    camera.lookAt(0, 0, 0)
+
+    var renderer = new THREE.WebGLRenderer()
+    renderer.setSize(width, height)
+
+    return {
+      renderer: renderer,
+      camera: camera,
+      scene: scene,
+      width: 800,
+      height: 600
+    }
+  },
+  addText () {
   },
   mounted () {
     var THREE = this.$THREE
 
-    var scene = new THREE.Scene()
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    var scene = this.scene
+    var renderer = this.renderer
+    var camera = this.camera
 
-    var renderer = new THREE.WebGLRenderer()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
+    var fontjson = require('./helvetiker_bold.typeface.json')
+    var font = new THREE.Font(fontjson)
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1)
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    var cube = new THREE.Mesh(geometry, material)
-    scene.add(cube)
+    var material = new THREE.MeshPhongMaterial({
+      color: 0x44f4f4,
+      specular: 0x44f4f4
+    })
 
-    camera.position.z = 5
+    var geometry = new THREE.TextGeometry('Hello three.js!', {
+      font: font,
+      size: 80,
+      height: 5,
+      curveSegments: 12,
+      bevelEnabled: true,
+      bevelThickness: 10,
+      bevelSize: 8,
+      bevelOffset: 0,
+      bevelSegments: 5
+    })
+
+    geometry.center()
+
+    var mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
+
+    this.$refs['threejs'].appendChild(renderer.domElement)
 
     function animate () {
       requestAnimationFrame(animate)
+      mesh.rotateY(0.1)
       renderer.render(scene, camera)
     }
     animate()
--- src/main.js
+++ src/main.js
@@ -3,16 +3,68 @@
 import Vue from 'vue'
 import App from './App'
 import Basic from './components/Basic.vue'
+import WebAudioTinySynth from 'webaudio-tinysynth'
+import WebMidi from 'webmidi'
 
 import * as THREE from 'three'
 Object.defineProperty(Vue.prototype, '$THREE', {value: THREE})
 
-Vue.config.productionTip = true
 Vue.component('basic', Basic)
+
+var notes = {}
+
+function midiNumberToNote (number) {
+  // midi note 60 is C4, and 72 is C5
+  var names = [
+    'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C'
+  ]
+
+  number = 1 * number
+  var octave = Math.floor(number / 12)
+  var key = names[number - octave * 12]
+  return {
+    octave: octave,
+    key: key
+  }
+}
+
+Object.defineProperty(Vue.prototype, '$notes', {value: notes})
+
+Vue.config.productionTip = true
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
+})
+
+var synth = new WebAudioTinySynth({
+  quality: 1,
+  useReverb: 0,
+  voices: 32
+})
+
+function noteOn (evt) {
+  console.log(evt.note.name + evt.note.octave)
+  synth.noteOn(evt.channel, evt.note.number, evt.rawVelocity)
+  notes[evt.note.number] = evt.rawVelocity
+}
+
+function noteOff (evt) {
+  synth.noteOff(evt.channel, evt.note.number)
+  notes[evt.note.number] = 0
+}
+
+WebMidi.enable(function (err) {
+  if (err) {
+    console.error('WebMidi enable error:', err)
+  } else {
+    // console.log(WebMidi.inputs)
+    // console.log(WebMidi.outputs)
+
+    var input = WebMidi.getInputByName('Midi Through Port-0')
+    input.addListener('noteon', 'all', noteOn)
+    input.addListener('noteoff', 'all', noteOff)
+  }
 })
